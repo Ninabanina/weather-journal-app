34ee244026816d2bfbbe60d02f2fcd665b8ce8d1
Nina.Wan <nina.wan@august.com.au>
2021-02-21 14:31:41 +1100

    feat: build the UI

--- server.js
+++ server.js
@@ -2,32 +2,38 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
 const port = 8000;
 
-const server = app.listen(port, ()=>{ console.log(`running on localhost: ${port}`) });
+const server = app.listen(port, () => {
+  console.log(`running on localhost: ${port}`);
+});
 
 // GET route
-app.get('/all', (req, res) => {res.send(projectData)});
+app.get("/all", (req, res) => {
+  res.send(projectData);
+});
 
 // POST route
-app.post('/add', (req, res) => {projectData = req.body; res.send({ message: "Post received"})});
+app.post("/add", (req, res) => {
+  projectData = req.body;
+  res.send({ message: "Post received" });
+});
--- website/app.js
+++ website/app.js
@@ -1,20 +1,26 @@
 /* Global Variables */
 // Base URL
-const baseURL = 'http://api.openweathermap.org/data/2.5/weather';
+const baseURL = "http://api.openweathermap.org/data/2.5/weather";
 // API key
-const apiKey = '48fa907989fde7316aeffd34d3c4440c';
+const apiKey = "48fa907989fde7316aeffd34d3c4440c";
+// Weather Icon
+const iconSource = "http://openweathermap.org/img/wn/";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
 // Generate the All data
-function generateData(e){
-    const zip =  document.getElementById('zip').value;
-    const feelings = document.getElementById('feelings').value;
+function generateData(e) {
+  document.getElementById("error").classList.add("hidden");
+  document.getElementById("weather-data__container").classList.add("hidden");
 
-    getWeatherData(baseURL, zip, apiKey)
-    .then(function(WeatherData){
+  const zip = document.getElementById("zip").value;
+  const feelings = document.getElementById("feelings").value;
+
+  if (zip) {
+    getWeatherData(baseURL, zip, apiKey).then(function (WeatherData) {
+      if (WeatherData.cod === 200) {
         const temperature = WeatherData.main.temp;
         const city = WeatherData.name;
         const description = WeatherData.weather[0].description;
@@ -23,53 +29,78 @@ function generateData(e){
         const humidity = WeatherData.main.humidity;
         const country = WeatherData.sys.country;
         const Userfeelings = feelings;
-        postData('/add', {
-            temperature, 
-            city, 
-            description, 
-            icon, 
-            windSpeed,
-            humidity,
-            country,
-            Userfeelings
+        postData("/add", {
+          temperature,
+          city,
+          description,
+          icon,
+          windSpeed,
+          humidity,
+          country,
+          Userfeelings,
         });
         displayWeatherData();
-    })
-}
-
-//Get Weather Data by ZIP
-const getWeatherData = async (baseURL, zip, apiKey)=>{
-  const res = await fetch(`${baseURL}?zip=${zip}&appid=${apiKey}`)
-  try {
-    const data = await res.json();
-    return data;
-  }  catch(error) {
-    console.log("error", error);
+      } else {
+        document.getElementById("error").innerHTML = WeatherData.message;
+        document.getElementById("error").classList.remove("hidden");
+      }
+    });
+  } else {
+    document.getElementById("error").innerHTML = 'Please enter a ZIP Code.'
+    document.getElementById("error").classList.remove("hidden");
   }
 }
 
+//Get Weather Data by ZIP
+const getWeatherData = async (baseURL, zip, apiKey) => {
+  const res = await fetch(`${baseURL}?zip=${zip}&appid=${apiKey}&units=metric`);
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
+    document.getElementById("error").innerHTML = error.message;
+  }
+};
+
 // POST to server
 async function postData(url, data) {
-    await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify(data),
-    });
+  await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
 }
 
 // Display Weather Data
 const displayWeatherData = async () => {
-    const request = await fetch('/all');
-    try{
-      const allData = await request.json();
-      document.getElementById('date').innerHTML = newDate;
-      document.getElementById('temp').innerHTML = "City:" + allData.city;
-      document.getElementById('content').innerHTML ="temperature: " + allData.temperature + allData.city + allData.description + allData.icon + allData.windSpeed + allData.humidity + allData.country + allData.Userfeelings;
-  
-    }catch(error){
-      console.log("error", error);
-    }
-  }
+  const request = await fetch("/all");
 
-document.getElementById('generate').addEventListener('click', generateData);
+  try {
+    const allData = await request.json();
+    document.getElementById("city").innerHTML = allData.city;
+    document.getElementById("temp").innerHTML = `${Math.floor(
+      allData.temperature
+    )}°C`;
+    document.getElementById(
+      "icon"
+    ).innerHTML = `<img src="${iconSource}${allData.icon}@2x.png"></img>`;
+    document.getElementById("description").innerHTML = allData.description;
+    document.getElementById("date").innerHTML = `Today is: ${newDate}`;
+    if (allData.Userfeelings) {
+      document.getElementById(
+        "content"
+      ).innerHTML = `Feeling: ${allData.Userfeelings}`;
+    }
+    document
+      .getElementById("weather-data__container")
+      .classList.remove("hidden");
+  } catch (error) {
+    console.log("error", error);
+    document.getElementById("error").innerHTML = 'Sorry something went wrong. please try again.'
+    document.getElementById("error").classList.remove("hidden");
+  }
+};
+
+document.getElementById("generate").addEventListener("click", generateData);
